19c3b985c039eb3ad0f993da204864f684e71dea
quakex <iquakex@gmail.com>
2016-07-09 19:14:54 +0800

    finish 7 Refactor Code

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,9 @@
 module GroupsHelper
+  def render_group_title(group)
+    simple_format(truncate(group.title, lenth:20))
+  end
+
+  def render_group_content(group)
+    simple_format(truncate(group.description, lenth:100))
+  end
 end
